8a3973ce923a6b9cd51ba60b5198c49082ae7d28
tbzz83 <tbzz83@gmail.com>
2025-10-21 14:20:19 -0400

    Experimenting

--- cmd/gg/tmp/main.go
+++ cmd/gg/tmp/main.go
@@ -1,77 +1,36 @@
 package main
 
-// A simple program demonstrating the text input component from the Bubbles
-// component library.
-
 import (
 	"fmt"
-	"log"
+	"strings"
+	"time"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
+	text := "hello world"
+	cursor := 5
 
-type (
-	errMsg error
-)
+	blockStyle := lipgloss.NewStyle().
+		Background(lipgloss.Color("12")).
+		Foreground(lipgloss.Color("0"))
 
-type model struct {
-	textInput textinput.Model
-	err       error
-}
+	for {
+		fmt.Print("\033[H\033[2J") // clear screen
 
-func ValidateFunc() {
-
-}
-
-func initialModel() model {
-	ti := textinput.New()
-	ti.Placeholder = "Pikachu"
-	ti.Focus()
-	ti.CharLimit = 156
-	ti.Width = 20
-
-	return model{
-		textInput: ti,
-		err:       nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textinput.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
+		// Draw text with block cursor
+		var builder strings.Builder
+		builder.WriteString(text[:cursor])
+		if cursor < len(text) {
+			builder.WriteString(blockStyle.Render(string(text[cursor])))
+			builder.WriteString(text[cursor+1:])
+		} else {
+			builder.WriteString(blockStyle.Render(" "))
 		}
 
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		return m, nil
+		fmt.Println(builder.String())
+		time.Sleep(200 * time.Millisecond)
 	}
-
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
 }
 
-func (m model) View() string {
-	return fmt.Sprintf(
-		"What’s your favorite Pokémon?\n\n%s\n\n%s",
-		m.textInput.View(),
-		"(esc to quit)",
-	) + "\n"
-}
